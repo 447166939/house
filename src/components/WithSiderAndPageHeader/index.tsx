c5dee3f3d3c3afb8bf82250f9220d0c974864436
王永星 <447166939@qq.com>
2021-06-04 03:05:09 +0800

    update

--- src/components/WithSiderAndPageHeader/index.tsx
+++ src/components/WithSiderAndPageHeader/index.tsx
@@ -1,46 +1,48 @@
-import React from 'react'
+import React from "react";
 import { makeStyles, createStyles } from "@material-ui/core";
 import Layout from "@/components/Layout";
 import Sider from "@/components/Sider";
 import PageHeader from "@/components/PageHeader";
 export interface IWithSiderAndPageHeaderProps {
- children:JSX.Element | JSX.Element[];
+  children: JSX.Element | JSX.Element[];
 }
-const useStyles=makeStyles(theme=>createStyles({
+const useStyles = makeStyles((theme) =>
+  createStyles({
     root: {
-        paddingLeft: "276px",
-        paddingRight: "276px",
-        alignSelf: "stretch"
+      paddingLeft: "276px",
+      paddingRight: "276px",
+      alignSelf: "stretch"
     },
     container: {
-        display: "flex",
-        marginTop: "20px"
+      display: "flex",
+      marginTop: "20px"
     },
     siderWrapper: {
-        width: "320px",
-        marginRight: "20px"
+      width: "320px",
+      marginRight: "20px"
     },
     content: {
-        flex: 1
-    },
-}))
-const WithSiderAndPageHeader:React.FC<IWithSiderAndPageHeaderProps>=(props:IWithSiderAndPageHeaderProps)=>{
-    const classes=useStyles()
-    const {children}=props
-    return (
-        <Layout>
-            <div className={classes.root}>
-                <PageHeader />
-                <div className={classes.container}>
-                    <div className={classes.siderWrapper}>
-                        <Sider/>
-                    </div>
-                    <div className={classes.content}>
-                        {children}
-                </div>
-            </div>
+      flex: 1
+    }
+  })
+);
+const WithSiderAndPageHeader: React.FC<IWithSiderAndPageHeaderProps> = (
+  props: IWithSiderAndPageHeaderProps
+) => {
+  const classes = useStyles();
+  const { children } = props;
+  return (
+    <Layout>
+      <div className={classes.root}>
+        <PageHeader />
+        <div className={classes.container}>
+          <div className={classes.siderWrapper}>
+            <Sider />
+          </div>
+          <div className={classes.content}>{children}</div>
         </div>
-</Layout>
-)
-}
-export default WithSiderAndPageHeader
+      </div>
+    </Layout>
+  );
+};
+export default WithSiderAndPageHeader;
